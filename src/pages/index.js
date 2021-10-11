7830214e49cf067a23731273e80ceef93f8637c5
Walfie <walfington@gmail.com>
2021-10-10 23:29:56 -0400

    Use react-photo-gallery

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
-import Gallery from "react-grid-gallery";
+import React, { useState, useCallback } from "react";
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
 import tweets from "../tweets.json";
 import users from "../users.json";
 
@@ -7,21 +9,27 @@ const images = tweets.flatMap((tweet) => {
   return tweet.images.map((image) => {
     return {
       src: image.url,
-      thumbnail: image.url,
-      caption: tweet.text,
-      thumbnailWidth: image.width,
-      thumbnailHeight: image.height,
-      tags: [
-        {
-          title: "username",
-          value: `@${users[tweet.user_id]}`,
-        },
-      ],
+      width: image.width,
+      height: image.height,
+      title: `@${users[tweet.user_id]}`,
     };
   });
 });
 
 export default function Home() {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((_event, { index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
   return (
     <div className="container" style={{ padding: 0, margin: 0 }}>
       <Head>
@@ -30,7 +38,21 @@ export default function Home() {
       </Head>
 
       <main style={{ padding: 0, margin: 0 }}>
-        <Gallery images={images} enableImageSelection={false} />
+        <Gallery photos={images} onClick={openLightbox} />
+        <ModalGateway>
+          {viewerIsOpen ? (
+            <Modal onClose={closeLightbox}>
+              <Carousel
+                currentIndex={currentImage}
+                views={images.map((img) => ({
+                  ...img,
+                  srcset: img.srcSet,
+                  caption: img.title,
+                }))}
+              />
+            </Modal>
+          ) : null}
+        </ModalGateway>
       </main>
     </div>
   );
