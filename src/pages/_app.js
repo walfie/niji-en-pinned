d3764f1ced499486e71bf6b73bb7280a5f38bc0d
Walfie <walfington@gmail.com>
2021-10-12 23:18:59 -0400

    Add meta tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,22 @@
 import "../../styles/globals.css";
 import Head from "next/head";
 
+const title = "Nijisanji EN Pinned Tweets";
+const description = "Image gallery of Nijisanji EN pinned tweets";
+
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Nijisanji EN Pinned Tweets</title>
+        <title>{title}</title>
         <link rel="icon" href="./favicon.svg" />
+
+        <meta name="title" content={title} />
+        <meta name="description" content={description} />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
       </Head>
 
       <Component {...pageProps} />
